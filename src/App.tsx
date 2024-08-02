2d5fb2c459a56fd1904e7ad3fa87b6b530b9ce79
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-08-02 16:18:01 +0500

    Adding Basic Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import Weather from './Weather';
 import Location from './Location';
+import About from './About';
+import Help from './Help';
+import { Routes, Route, Link } from 'react-router-dom';
+
 const App: React.FC = (): React.ReactElement => {
     // const [selectedLoc, setSelectedLoc]=useState('Lahore');
     const [selectedLoc, setSelectedLoc] = useState<string>('Lahore');
@@ -15,10 +19,26 @@ const App: React.FC = (): React.ReactElement => {
     
     return (
         <div>
-            <Location onTogal={handleChange}/>
-            {/* <Weather location={`${selectedLoc}`}/> */}
-            {/* <Weather location={`${selectedLoc}`} loading={loading} setLoading={setLoading}/> */}
-            <Weather location={selectedLoc}/>
+            <h2 className='text-center py-5 bg-slate-900 font-poppins font-bold text-3xl'>Weather App</h2>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/help">Help</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path='/' element={<>
+                    <Location onTogal={handleChange}/>
+                    {/* <Weather location={`${selectedLoc}`}/> */}
+                    {/* <Weather location={`${selectedLoc}`} loading={loading} setLoading={setLoading}/> */}
+                    <Weather location={selectedLoc}/>
+                </>}>
+                </Route>
+                <Route path='/about' element={<About/>}></Route>
+                <Route path='/help' element={<Help/>}></Route>
+            </Routes>
+
         </div>
     );
 };
